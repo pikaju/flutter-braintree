d9a68453a0446cd4e6657c012b26cdb4bbc4e656
Julien Scholz <julienscholzo@hotmail.de>
2019-11-07 19:03:53 +0100

    Improve example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_braintree/flutter_braintree.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+        home: MyApp(),
+      ),
+    );
 
 class MyApp extends StatefulWidget {
   @override
@@ -9,60 +13,62 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _text = "";
+  void showNonce(BraintreePaymentMethodNonce nonce) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Payment method nonce:'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text('Nonce: ${nonce.nonce}'),
+            SizedBox(height: 16),
+            Text('Type label: ${nonce.typeLabel}'),
+            SizedBox(height: 16),
+            Text('Description: ${nonce.description}'),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Braintree example app'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () async {
-                  setState(() {
-                    _text = 'Waiting for result...';
-                  });
-                  var request = BraintreeDropInRequest(
-                    tokenizationKey: 'sandbox_8hxpnkht_kzdtzv2btm4p7s5j',
-                    collectDeviceData: true,
-                    googlePaymentRequest: BraintreeGooglePaymentRequest(
-                      totalPrice: '4.20',
-                      currencyCode: 'USD',
-                      billingAddressRequired: false,
-                    ),
-                    paypalRequest: BraintreePayPalRequest(
-                      amount: '4.20',
-                      displayName: 'Example company',
-                    ),
-                  );
-                  BraintreeDropInResult result =
-                      await BraintreeDropIn.start(request);
-                  setState(() {
-                    if (result == null) {
-                      _text = 'Selection canceled';
-                    } else {
-                      _text = 'Nonce: ${result.paymentMethodNonce.nonce}\n\n'
-                          'Nonce label: ${result.paymentMethodNonce.typeLabel}\n\n'
-                          'Nonce description: ${result.paymentMethodNonce.description}\n\n'
-                          'Device data: ${result.deviceData}';
-                    }
-                  });
-                },
-                child: Text(
-                  'SELECT PAYMENT METHOD',
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Braintree example app'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () async {
+                var request = BraintreeDropInRequest(
+                  tokenizationKey: 'sandbox_8hxpnkht_kzdtzv2btm4p7s5j',
+                  collectDeviceData: true,
+                  googlePaymentRequest: BraintreeGooglePaymentRequest(
+                    totalPrice: '4.20',
+                    currencyCode: 'USD',
+                    billingAddressRequired: false,
+                  ),
+                  paypalRequest: BraintreePayPalRequest(
+                    amount: '4.20',
+                    displayName: 'Example company',
+                  ),
+                );
+                BraintreeDropInResult result =
+                    await BraintreeDropIn.start(request);
+                if (result != null) {
+                  showNonce(result.paymentMethodNonce);
+                }
+              },
+              child: Text(
+                'SELECT PAYMENT METHOD',
               ),
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(_text, textAlign: TextAlign.center),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
