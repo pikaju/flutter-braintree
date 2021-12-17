87f24b39051531e9170ceec72ab4a15169aac3b0
Nabin Adhikari <nabin@stridesolutions.com.au>
2021-12-17 15:24:44 +1100

    same for ios

--- ios/Classes/FlutterBraintreeCustomPlugin.swift
+++ ios/Classes/FlutterBraintreeCustomPlugin.swift
@@ -40,7 +40,24 @@ public class FlutterBraintreeCustomPlugin: BaseFlutterBraintreePlugin, FlutterPl
                 paypalRequest.currencyCode = requestInfo["currencyCode"] as? String
                 paypalRequest.displayName = requestInfo["displayName"] as? String
                 paypalRequest.billingAgreementDescription = requestInfo["billingAgreementDescription"] as? String
-                
+                if let intent = requestInfo["payPalPaymentIntent"] as? String {
+                    switch intent {
+                    case "order":
+                        paypalRequest.intent = BTPayPalRequestIntent.order
+                    case "sale":
+                        paypalRequest.intent = BTPayPalRequestIntent.sale
+                    default:
+                        paypalRequest.intent = BTPayPalRequestIntent.authorize
+                    }
+                }
+                if let userAction = requestInfo["payPalPaymentUserAction"] as? String {
+                    switch userAction {
+                    case "commit":
+                        paypalRequest.userAction = BTPayPalRequestUserAction.commit
+                    default:
+                        paypalRequest.userAction = BTPayPalRequestUserAction.default
+                    }
+                }
                 driver.tokenizePayPalAccount(with: paypalRequest) { (nonce, error) in
                     self.handleResult(nonce: nonce, error: error, flutterResult: result)
                     self.isHandlingResult = false
@@ -49,7 +66,24 @@ public class FlutterBraintreeCustomPlugin: BaseFlutterBraintreePlugin, FlutterPl
                 let paypalRequest = BTPayPalVaultRequest()
                 paypalRequest.displayName = requestInfo["displayName"] as? String
                 paypalRequest.billingAgreementDescription = requestInfo["billingAgreementDescription"] as? String
-                
+                if let intent = requestInfo["payPalPaymentIntent"] as? String {
+                    switch intent {
+                    case "order":
+                        paypalRequest.intent = BTPayPalRequestIntent.order
+                    case "sale":
+                        paypalRequest.intent = BTPayPalRequestIntent.sale
+                    default:
+                        paypalRequest.intent = BTPayPalRequestIntent.authorize
+                    }
+                }
+                if let userAction = requestInfo["payPalPaymentUserAction"] as? String {
+                    switch userAction {
+                    case "commit":
+                        paypalRequest.userAction = BTPayPalRequestUserAction.commit
+                    default:
+                        paypalRequest.userAction = BTPayPalRequestUserAction.default
+                    }
+                }
                 driver.tokenizePayPalAccount(with: paypalRequest) { (nonce, error) in
                     self.handleResult(nonce: nonce, error: error, flutterResult: result)
                     self.isHandlingResult = false
